21bcfa06489b99817f7e14e0470c2aee6192ae5c
skxxy <xiaoyixiong2023@u.northwestern.edu>
2023-01-11 21:20:31 -0600

    practice exercise lab4

--- lab-4-2.sql
+++ lab-4-2.sql
@@ -1,6 +1,18 @@
 -- What are the first and last names of the players who 
 -- played for the 2020 Chicago Cubs?
+SELECT players.first_name, players.last_name 
+FROM stats
+    INNER JOIN players ON players.id  = stats.player_id
+    INNER JOIN teams ON teams.id = stats.team_id
+WHERE teams.name = "Chicago Cubs"
+AND teams.year = 2020;
 
+-- SELECT players.first_name, players.last_name
+-- FROM stats 
+--   INNER JOIN teams ON teams.id = stats.team_id
+--   INNER JOIN players ON players.id = stats.player_id
+-- WHERE teams.year = 2020
+-- AND teams.name = "Chicago Cubs";
 -- Hint: combine WHERE clauses using AND, e.g.
 -- WHERE column1 = expression1
 -- AND column2 = expression2
